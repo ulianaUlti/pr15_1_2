f3dfbac07d15d89d5f2f8e206ea8857f7f03950c
ulianaUlti <124128505+ulianaUlti@users.noreply.github.com>
2023-05-17 16:52:42 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _15_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 5;
+            int b = 9;
+            Queue<int> fromAToB = new Queue<int>();
+            Queue<int> lessThenA = new Queue<int>();
+            Queue<int> moreThenB = new Queue<int>();
+            int number;
+            using (StreamReader reader = new StreamReader(@"d:\t1.txt", Encoding.Default))
+            {
+                while (!reader.EndOfStream)
+                {
+                    number = int.Parse(reader.ReadLine());
+                    if (number < a)
+                        lessThenA.Enqueue(number);
+                    else if (number > b)
+                        moreThenB.Enqueue(number);
+                    else
+                        fromAToB.Enqueue(number);
+                }
+            }
+            MessageBox.Show(string.Join(" ", fromAToB)label1);
+            MessageBox.Show(string.Join(" ", lessThenA));
+            MessageBox.Show(string.Join(" ", moreThenB));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace _15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str, newstr, newrts = null;
+            string glasnie = "аиоыуэяюё";
+            char[] s = glasnie.ToCharArray();
+            Console.WriteLine("Введите текст для записи в файл ");
+            str = Console.ReadLine();
+            FileStream file1 = new FileStream("C:\\new_file.txt", FileMode.Create);
+            StreamWriter writer = new StreamWriter(file1);
+            writer.Write(str);
+            writer.Close();
+            FileStream file2 = new FileStream("C:\\new_file.txt", FileMode.Open);
+            StreamReader reader = new StreamReader(file2);
+            newstr = (reader.ReadToEnd());
+            for (int i = 0; i < newstr.Length; i++)
+                for (int j = 0; j < s.Length; j++)
+                    if (newstr[i] == s[j])
+                        newrts = newrts + newstr[i];
+            reader.Close();
+            char[] v = newrts.ToCharArray();
+            Array.Reverse(v);
+            Console.WriteLine("Гласные файла в обратном порядке: ");
+            for (int i = 0; i < v.Length; i++)
+                Console.Write(v[i] + " ");
+            Console.ReadKey();
+        }
+    }
+}
